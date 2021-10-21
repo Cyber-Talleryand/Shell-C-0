f0d1a46f7ca2ef2bab379a8bf1481b369234d2f4
Cyber-Talleyrand <82671518+Cyber-Talleyrand@users.noreply.github.com>
2021-10-21 09:25:26 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Shell.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Shell.dir/dynamic_list.c.o"
+  "CMakeFiles/Shell.dir/main.c.o"
+  "Shell"
+  "Shell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Shell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
